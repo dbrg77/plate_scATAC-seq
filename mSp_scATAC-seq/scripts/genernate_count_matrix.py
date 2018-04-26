cd49029b46481aebfa295e82827fdb54ff1318c7
Xi Chen <dbrg77@gmail.com>
2018-04-26 13:15:11 +0100

    changed snakefile

--- mSp_scATAC-seq/scripts/genernate_count_matrix.py
+++ mSp_scATAC-seq/scripts/genernate_count_matrix.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from glob import iglob
+from scipy.io import mmwrite
+from scipy.sparse import csr_matrix
 
 scs = iglob("*/count/*.count")
 dfs = []
@@ -10,4 +12,15 @@ for df in scs:
     dfs.append(pd.read_table(df, header=None, index_col=0, names=["pid", cell]))
 
 matrix = pd.concat(dfs, axis=1)
-matrix.to_csv('count_matrix_over_aggregate_narrowPeaks.csv')
+# matrix.to_csv('count_matrix_over_aggregate_narrowPeaks.csv')
+mtx = csr_matrix(matrix)
+mmwrite('mSp_scATAC_count_matrix_over_aggregate.mtx',
+        mtx, field='integer')
+
+with open('mSp_scATAC_count_matrix_over_aggregate.rownames', 'w') as f:
+    f.write('\n'.join(matrix.index.values))
+    f.write('\n')
+
+with open('mSp_scATAC_count_matrix_over_aggregate.colnames', 'w') as f:
+    f.write('\n'.join(matrix.columns.values))
+    f.write('\n')
