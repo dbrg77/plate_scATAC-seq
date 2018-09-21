8f750c29335b75b79eb3751b9ce53084ce886732
dbrg77 <dbrg77@gmail.com>
2018-09-21 09:18:26 +0100

    corrected an error in the script

--- buenrostro_c1/scripts/get_depth_mr.sh
+++ buenrostro_c1/scripts/get_depth_mr.sh
@@ -3,7 +3,7 @@
 echo -e "cell\tsequencing_depth" > qc_metrics/sequencing_depth.txt
 echo -e "cell\tmapping_rate" > qc_metrics/mapping_rate.txt
 
-for i in */*/hisat2_*_log/*_aln_sum.txt; do
+for i in */*/hisat2_log/*_aln_sum.txt; do
     depth=$(grep 'reads; of these:' $i | awk '{print $1}')
     mr=$(grep overall ${i} | awk '{print $1}' | cut -f 1 -d%)
     cell=$(echo ${i} | rev | cut -f 1 -d/ | rev)
