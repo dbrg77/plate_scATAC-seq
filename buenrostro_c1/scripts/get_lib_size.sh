a3379d322602f323b54e205f2f2ab8dad60396ce
dbrg77 <dbrg77@gmail.com>
2018-09-22 21:47:59 +0100

    corrected an error in the get_lib_size script

--- buenrostro_c1/scripts/get_lib_size.sh
+++ buenrostro_c1/scripts/get_lib_size.sh
@@ -3,7 +3,7 @@
 echo -e "cell\tdup_level" > qc_metrics/library_size.txt
 
 for i in */*/picard_log/*.out; do
-    dup=$(grep Unknown ${i} | cut -f 10)
+    size=$(grep Unknown ${i} | cut -f 10)
     cell=$(echo ${i} | rev | cut -f 1 -d/ | rev)
-    echo -e "${cell%_f2q30_pmd.out}\t${dup}" >> qc_metrics/library_size.txt
+    echo -e "${cell%_f2q30_pmd.out}\t${size}" >> qc_metrics/library_size.txt
 done
--- downsampling/scripts/get_lib_size.sh
+++ downsampling/scripts/get_lib_size.sh
@@ -5,8 +5,8 @@ for i in */picard_log; do
     echo -e "cell\tdup_level" > ${frac}/qc_metrics/library_size.txt
 
     for j in ${i}/*.out; do
-        dup=$(grep Unknown ${j} | cut -f 10)
+        size=$(grep Unknown ${j} | cut -f 10)
         cell=$(echo ${j} | rev | cut -f 1 -d/ | rev)
-        echo -e "${cell%_f2q30_pmd.out}\t${dup}" >> ${frac}/qc_metrics/library_size.txt
+        echo -e "${cell%_f2q30_pmd.out}\t${size}" >> ${frac}/qc_metrics/library_size.txt
     done
 done
